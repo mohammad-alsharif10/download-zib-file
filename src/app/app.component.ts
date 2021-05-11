d526f11f810f961946271adbedb0749e4e1b5463
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2021-05-11 09:16:18 +0200

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as JSZip from 'jszip';
+import {saveAs} from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'download-zib-file';
+  zipFile: JSZip = new JSZip();
+
+  ngOnInit(): void {
+    this.zipFile.file('Hello.txt', 'Hello World\n');
+    this.zipFile.file('smile.txt', 'hello again');
+    this.zipFile.generateAsync({type: 'blob'})
+      .then((content) => {
+        // see FileSaver.js
+        saveAs(content, 'example.zip');
+      });
+  }
 }
